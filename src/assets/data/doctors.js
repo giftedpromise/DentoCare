95e362eee069f538e74627a38a5935bf483683f2
giftedpromise <giftedpromise1992@gmail.com>
2024-09-17 13:23:11 -1200

    doctors

--- src/assets/data/doctors.js
+++ src/assets/data/doctors.js
@@ -1,36 +1,36 @@
-import doctorImg01 from "../images/doctor-img01.png";
-import doctorImg02 from "../images/doctor-img02.png";
-import doctorImg03 from "../images/doctor-img03.png";
+import doctorImg01 from "../images/dentist1.jpeg";
+import doctorImg02 from "../images/dentist2.jpg";
+import doctorImg03 from "../images/dentist3.jpg";
 
 export const doctors = [
   {
     id: "01",
     name: "Dr. Alfaz Ahmed",
-    specialty: "Surgeon",
+    specialization: "Maxillofacial Surgeon",
     avgRating: 4.8,
-    totalRating: 272,
+    totalRating: 280,
     photo: doctorImg01,
-    totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    totalPatients: +1500,
+    hospital: "DentalMed, Nigeria.",
   },
   {
     id: "02",
-    name: "Dr. Saleh Mahmud",
-    specialty: "Neurologist",
-    avgRating: 4.8,
-    totalRating: 272,
+    name: "Dr. Esther Adekunle",
+    specialization: "Orthodontist",
+    avgRating: 4.9,
+    totalRating: 300,
     photo: doctorImg02,
     totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    hospital: "DentalMed, Nigeria.",
   },
   {
     id: "03",
-    name: "Dr. Farid Uddin",
-    specialty: "Dermatologist",
+    name: "Dr. Grace Okafor",
+    specialization: "Periodontist",
     avgRating: 4.8,
     totalRating: 272,
     photo: doctorImg03,
-    totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    totalPatients: +1500,
+    hospital: "DentalMed, Nigeria.",
   },
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,13 @@ import heroImg03 from "../assets/images/hero-img05.jpeg";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import videoIcon from "../assets/images/video-icon.png";
+import avatarIcon from "../assets/images/avatar.jpeg";
 import { Link } from "react-router-dom";
 import FeatureImg from "../assets/images/feature3.jpg";
 import { BsArrowRight } from "react-icons/bs";
 import ServicesList from "../components/Services/ServicesList";
+import DoctorList from "../components/Doctors/DoctorList";
 
 const Home = () => {
   return (
@@ -61,7 +64,7 @@ const Home = () => {
             <div className="flex gap-[30px] justify-end">
               <div>
                 <img
-                  className="w-[370px] h-[524px] object-cover"
+                  className="w-[370px] h-[524px] object-cover rounded"
                   src={heroImg01}
                   alt="hero iamge1"
                 />
@@ -106,7 +109,7 @@ const Home = () => {
                 </p>
                 <Link
                   to="/doctors"
-                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:borde-none"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                   <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
@@ -180,14 +183,12 @@ const Home = () => {
                 Get virtual consultation <br /> anytime.
               </h2>
               <ul className="pl-4">
+                <li className="text_para">Schedule the appointment directly</li>
                 <li className="text_para">
-                  1. Schedule the appointment directly
+                  Search for your Dentist here, and contact their office.
                 </li>
                 <li className="text_para">
-                  2. Serch for your Dentist here, and contact their office.
-                </li>
-                <li className="text_para">
-                  3. Check out dentists currently accepting new patients and use
+                  Check out dentists currently accepting new patients and use
                   the online scheduling tool to book your appointment.
                 </li>
               </ul>
@@ -199,10 +200,54 @@ const Home = () => {
               <img
                 src={FeatureImg}
                 alt=""
-                className="w-[370px] h-[370px] object-cover"
+                className="w-[470px] h-[470px] rounded object-cover"
               />
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue,24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <figure className="w-[40px] h-[40px] rounded-full cursor-pointer">
+                    <img
+                      src={avatarIcon}
+                      className="w-full rounded-full"
+                      alt="userimage"
+                    />
+                  </figure>
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Collins David
+                  </h4>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+      </section>
+      <section>
+        <div className="container lg:px-[48px]">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Great Doctors</h2>
+            <p className="text_para text-center">
+              World-class dental care for everyone. Our advanced dental
+              treatments offer unmatched precision and expertise
+            </p>
+          </div>
+
+          <DoctorList />
         </div>
       </section>
     </>
