446d9a52c9e0a450987a29051f71bd2e3fbf8fb1
giftedpromise <giftedpromise1992@gmail.com>
2024-09-21 01:16:42 +0100

    menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -83,17 +83,7 @@ const Header = () => {
               ))}
             </ul>
           </div>
-          <div className="flex items-center gap-4">
-            <Link to="/login">
-              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
-                Login
-              </button>
-            </Link>
-            <span className="md:hidden" onClick={toggleMenu}>
-              <BiMenu className="w-6 h-6 cursor-pointer" />
-            </span>
-          </div>
-        </div>
+         
       </div>
     </header>
   );
